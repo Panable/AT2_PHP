ba09e26c94deba3bcb55f910b671d9b0e6b3726c
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-09-08 23:55:39 +1000

    autoloading sorted :)

--- cheltonmvc/public/index.php
+++ cheltonmvc/public/index.php
@@ -1,16 +1,6 @@
 <?php
-// Include Composer's autoloader
 require '../../vendor/autoload.php';
 
-// Import the LoremIpsum class
-use joshtronic\LoremIpsum;
-use App\Models\Menu;
+use App\Models;
 
-$hi = new Menu();
-
-// Create an instance of the LoremIpsum class
-$loremIpsum = new LoremIpsum();
-
-// Generate lorem ipsum text
-$text = $loremIpsum->words(500); // Generate 5 words of lorem ipsum
-echo $text;
+$hi = new Models\Menu();
