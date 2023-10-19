4c7901a1c3a54190d99b3dd8c4f07d936ba3ea95
Dhanveer Ramnauth <dhanr1606@gmail.com>
2023-10-20 04:34:23 +1100

    h

--- cheltonmvc/app/config/config.php
+++ cheltonmvc/app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 //Database
-define('DB_HOST', 'at2_php-mysql-1');
+define('DB_HOST', 'at3_php-mysql-1');
 define('DB_USER', 'root');
 define('DB_PASS', 'password');
 define('DB_NAME', 'cafe');
